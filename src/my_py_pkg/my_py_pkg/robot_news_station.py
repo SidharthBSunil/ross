a900f7c6c1733a7534f2ffe0f7542103bcb36825
SidharthBSunil <sidharthbsunil@gmail.com>
2025-07-24 22:08:26 +0530

    publish 1

--- src/my_py_pkg/my_py_pkg/robot_news_station.py
+++ src/my_py_pkg/my_py_pkg/robot_news_station.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from example_interfaces.msg import String
 
-class MyCustomNode(Node):
+class RobotNewsStation(Node):
 
     def __init__(self):
-        super().__init__("py_test")
-        
+        super().__init__("robot_news_station")
+        self.publisher_= self.create_publisher(String,"robot_news",10)#10 messages
+        self.timer = self.create_timer(0.5, self.publish_news)
+        self.get_logger().info("Robot News Station has been started.")
 
-    def timer_callback(self):
-        self.get_logger().info("Hello"+ str(self.counter))
-        self.counter+=1    
+    def publish_news(self):
+        msg = String()
+        msg.data="Hello" 
+        self.publisher_.publish(msg) 
 
 def main(args=None):
     rclpy.init(args=args)
-    node=MyCustomNode()
+    node=RobotNewsStation()
     rclpy.spin(node)
     rclpy.shutdown()
 
-    rclpy.shutdown()
+    
 if __name__ == "__main__":
     main()    
